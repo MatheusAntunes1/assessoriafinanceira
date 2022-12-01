d3bd9bc72f21812efa22fa826cf74be8540a3b1c
Matheus Antunes <antunes1matheus@gmail.com>
2022-12-01 14:15:49 -0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// banner
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 4000);
+
+function nextImage(){
+    count++
+    if(count>4){
+        count=1;
+    }
+
+
+    document.getElementById("radio"+count).checked = true;
+}
+
+// menu hamburguer
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event){
+
+    if(event.type === "touchstart") event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
